6fafd67540b0efd8e791d521b2adb63fe1c8c115
Henrik <Henrik.eide@student.uib.no>
2020-08-17 12:33:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fapp/FappActivity.java
@@ -0,0 +1,50 @@
+package com.example.fapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FappActivity extends AppCompatActivity {
+    private TextView message;
+    private int counter = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message = findViewById(R.id.clickCounter);
+        ImageView cretin = findViewById(R.id.cretinPic);
+
+        //Define and attach click listener
+        cretin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tapCretin();
+            }
+        });
+    }
+    private void tapCretin() {
+        counter++;
+        String countAsText;
+        /*
+         * In real applications you should not write switch like the one below.
+         * Use resource of type "Quantity strings (plurals)" instead.
+         * See https://developer.android.com/guide/topics/resources/string-resource#Plurals
+         */
+        switch (counter) {
+            case 1:
+                countAsText = "once";
+                break;
+            case 2:
+                countAsText = "twice";
+                break;
+            default:
+                countAsText = String.format("%d times", counter);
+        }
+        message.setText(String.format("You touched the bug %s", countAsText));
+    }
+}
